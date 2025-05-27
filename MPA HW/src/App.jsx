24545a73389c3a6121edb3f87329062902760650
Omar Salloum <omarsalloum08@gmail.com>
2025-05-27 19:48:34 +0200

    adding scaffolding

--- MPA HW/src/App.jsx
+++ MPA HW/src/App.jsx
@@ -1,34 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header/Header"
+import ArticlesPage from "./pages/ArticlesPage/ArticlesPage";
+import ArticleDetailsPage from "./pages/ArticleDetailsPage/ArticleDetailsPage";
+import NewArticlePage from "./pages/NewArticlePage/NewArticlePage";
+import EditArticlePage from "./pages/EditArticlePage/EditArticlePage";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/articles" />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/articles/:id" element={<ArticleDetailsPage />} />
+        <Route path="/articles/:id/modify" element={<EditArticlePage />} />
+        <Route path="/new-article" element={<NewArticlePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
